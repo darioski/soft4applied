bc7bee542d948e1bf24125b7ee4e15ef44184e15
Dario Verna <dario.verna@studio.unibo.it>
2022-10-04 20:06:47 +0200

    time evolution added

--- myfunctions.py
+++ myfunctions.py
@@ -3,5 +3,29 @@ import numpy as np
 import pytest
 
 
+def potential_operator(psi, pot, dt):
+    return psi * np.exp(-0.5j * dt * pot)
+
+
+def kinetic_operator(phi, k, dt):
+    return phi * np.exp(-0.5j * dt * k ** 2)
+
+
+def time_step(psi, pot, k, dt):
+    
+    psi = potential_operator(psi, pot, dt)  # apply operator V/2
+    phi = np.fft.fft(psi, axis=0)   # fft to reciprocal space
+    phi = kinetic_operator(phi, k, dt)  # apply operator T
+    psi = np.fft.ifft(phi, axis=0)  # inverse fft to real space
+    psi = potential_operator(psi, pot, dt)  # apply operator V/2
+    phi = np.fft.fft(psi, axis=0)   # fft
+    
+    return psi, phi
+
+
+
+
+
+
 # ----------- tests ------------
 
--- run.py
+++ run.py
@@ -4,31 +4,48 @@ from myfunctions import *
 import pytest
 
 # read parameters from file
-le, n = np.loadtxt('input', usecols=2, unpack=True)
+n, m = np.loadtxt('input', usecols=2, unpack=True)
 n = int(n)
+m = int(m)
+dt = 1. / m
 
 # x-space
-x = np.linspace(0, le, n, endpoint=False)
+x = np.linspace(0., 1., n, endpoint=False)
+
+# potential
+pot = np.zeros(n)
 
 # reciprocal space
-k = 2 * np.pi * np.fft.fftfreq(n, d=le/n)
+k = 2 * np.pi * np.fft.fftfreq(n, d=1./n)
 
 # read parameters for wave packet
-sigma = 1
-a = 1 / (2 * np.pi * sigma ** 2) ** 0.25    # normalization
-x_0 = 10    # initial position
-k_0 = 10    # initial momentum
+sigma = 0.01
+a = 1. / (2 * np.pi * sigma ** 2) ** 0.25    # normalization
+x_0 = 0.1    # initial position
+k_0 = 1000   # initial momentum
 
 # define wave functions
-psi = a * np.exp(1j * k_0 * x - ((x - x_0) / (2 * sigma)) ** 2)
-phi = np.fft.fft(psi)
+psi = np.zeros((n, m+1), dtype=complex)
+phi = np.zeros((n, m+1), dtype=complex)
 
+psi[:, 0] = a * np.exp(1j * k_0 * x - ((x - x_0) / (2 * sigma)) ** 2)
+phi[:, 0] = np.fft.fft(psi[:, 0])
+
+# evolve
+for i in range(m):
+    psi[:, i+1], phi[:, i+1] = time_step(psi[:, i], pot, k, dt) 
+    
 
 # compute squared module
-psi_2 = np.abs(psi) ** 2
-phi_2 = le ** 2 / (2 * np.pi * n ** 2) * np.abs(phi) ** 2
+psi_2 = np.zeros((n, m+1))
+phi_2 = np.zeros((n, m+1))
+
+for i in range(m+1):
+    psi_2[:, i] = np.abs(psi[:, i]) ** 2
+    phi_2[:, i] = 1. / (2 * np.pi * n ** 2) * np.abs(phi[:, i]) ** 2
 
 # save in output files
+
 with open('psi_2.npy', 'wb') as f:
     np.save(f, psi_2)
 
@@ -44,9 +61,14 @@ def test_type():
     assert psi.dtype == 'complex128'
 
 def test_psi_normalization():
-    i = np.trapz(psi_2, x)
+    i = np.trapz(psi_2[:, 0], x)
     assert np.isclose(i, 1.)
 
 def test_phi_normalization():
-    i = np.trapz(phi_2, k)
+    i = np.trapz(phi_2[:, 0], k)
     assert np.isclose(i, 1.)
+
+def test_norm_conserved():
+    i_start = np.trapz(psi_2[:, 0], x)
+    i_end =  np.trapz(psi_2[:, -1], x)
+    assert np.isclose(i_start, i_end, atol=1e-5)
