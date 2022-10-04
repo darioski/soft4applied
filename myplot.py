27504f7dd9af64da15117c22bd1ae1cb7a99043d
Dario Verna <dario.verna@studio.unibo.it>
2022-10-04 20:15:50 +0200

    animation added

--- myplot.py
+++ myplot.py
@@ -2,21 +2,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # parameters (to be removed)
-le = 50
-n = 1024
+
+n, m = np.loadtxt('input', usecols=2, unpack=True)
+n = int(n)
+m = int(m)
 
 # spaces (to be removed)
-x = np.linspace(0, le, n, endpoint=False)
-k = 2 * np.pi * np.fft.fftfreq(n, d=le/n)
+x = np.linspace(0., 1., n, endpoint=False)
+k = 2 * np.pi * np.fft.fftfreq(n, d=1/n)
 
 # load data
 psi_2 = np.load('psi_2.npy')
 phi_2 = np.load('phi_2.npy')
 
 # plot
-fig, ax = plt.subplots(2, 1)
+plt.switch_backend('macosx')
+plt.ion()
 
-ax[0].plot(x, psi_2)
-ax[1].plot(k[:n//2], phi_2[:n//2])
+freq = 10
+fig, (ax1, ax2) = plt.subplots(2, 1)
 
+for i in range(0, m, freq):
+    ax1.clear()
+    ax2.clear()
+    
+    ax1.plot(x, psi_2[:, i])
+    ax2.plot(k, phi_2[:, i])
+    
+    plt.pause(0.01)
+    
+
+plt.ioff()
 plt.show()
